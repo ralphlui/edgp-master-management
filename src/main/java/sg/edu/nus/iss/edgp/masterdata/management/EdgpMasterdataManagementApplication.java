c360fe4767352e049e49f3e21f091ffa2883fa1a
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-06-23 00:52:26 +0800

    Initial commit: backend setup with database config, AWS secret loading, and security starter.

---
+++ src/main/java/sg/edu/nus/iss/edgp/masterdata/management/EdgpMasterdataManagementApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.edgp.masterdata.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgpMasterdataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgpMasterdataManagementApplication.class, args);
+	}
+
+}
