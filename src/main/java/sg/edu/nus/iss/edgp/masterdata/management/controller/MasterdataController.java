e8fda765f189c0acb33e9f8f4430583eeaefd989
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-07-10 20:37:27 +0800

    Add authorization header in uploadCsvDataToTable audit log.

--- src/main/java/sg/edu/nus/iss/edgp/masterdata/management/controller/MasterdataController.java
+++ src/main/java/sg/edu/nus/iss/edgp/masterdata/management/controller/MasterdataController.java
@@ -10,23 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import sg.edu.nus.iss.edgp.masterdata.management.dto.APIResponse;
-import sg.edu.nus.iss.edgp.masterdata.management.dto.AuditDTO;
-import sg.edu.nus.iss.edgp.masterdata.management.dto.SearchRequest;
-import sg.edu.nus.iss.edgp.masterdata.management.enums.AuditLogInvalidUser;
-import sg.edu.nus.iss.edgp.masterdata.management.enums.HTTPVerb;
+import sg.edu.nus.iss.edgp.masterdata.management.dto.*;
+import sg.edu.nus.iss.edgp.masterdata.management.enums.*;
 import sg.edu.nus.iss.edgp.masterdata.management.exception.MasterdataServiceException;
 import sg.edu.nus.iss.edgp.masterdata.management.pojo.UploadRequest;
 import sg.edu.nus.iss.edgp.masterdata.management.service.impl.AuditService;
@@ -69,7 +59,7 @@ public class MasterdataController {
 			message = masterdataService.uploadCsvDataToTable(file, uploadReq,authorizationHeader);
 			if (message.equals("")) {
 				message = "Upload failed due to incorrect column format or missing values.";
-				auditService.logAudit(auditDTO, 500, "", "");
+				auditService.logAudit(auditDTO, 500, message, authorizationHeader);
 				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(APIResponse.error(message));
 
 			}
